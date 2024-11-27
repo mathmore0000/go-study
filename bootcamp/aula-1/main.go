b7aba90c4c6b562731e5020eb37a01494fb4413e
Matheus Moreira <matheusmoreira2004@live.com>
2024-11-27 11:40:24 -0300

    feat: added aula-1 dir

---
+++ bootcamp/aula-1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"module/soma"
+)
+
+func main() {
+	fmt.Print("Hello World!\n")
+
+	fmt.Println(soma.Somar(1, 2))
+}
---
+++ bootcamp/aula-1/soma/soma.go
@@ -0,0 +1,5 @@
+package soma
+
+func Somar(n1 int, n2 int) (soma int) {
+	return n1 + n2
+}
